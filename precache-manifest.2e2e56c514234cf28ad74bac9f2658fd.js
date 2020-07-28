1081673c62ccf758c38c450f209ce20bc9fecb4e
wisdomosara <wisdomosara@gmail.com>
2020-07-28 21:07:54 +0100

    Updates

---
+++ precache-manifest.2e2e56c514234cf28ad74bac9f2658fd.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fb5faf28fdaa0d3039a5b00a52e213",
+    "url": "/fmlapp/index.html"
+  },
+  {
+    "revision": "bcf4ac6ca1e03ab4699d",
+    "url": "/fmlapp/static/css/main.ae12349a.chunk.css"
+  },
+  {
+    "revision": "5dd489c0c011450b25e9",
+    "url": "/fmlapp/static/js/2.057020d3.chunk.js"
+  },
+  {
+    "revision": "05459a3c6db4c453e7d0d7b89b9cc7f1",
+    "url": "/fmlapp/static/js/2.057020d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcf4ac6ca1e03ab4699d",
+    "url": "/fmlapp/static/js/main.60199b89.chunk.js"
+  },
+  {
+    "revision": "afe68ac06c48d95595975c9c465c41f8",
+    "url": "/fmlapp/static/js/main.60199b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b1025834480e8b7c90",
+    "url": "/fmlapp/static/js/runtime-main.f68f0544.js"
+  },
+  {
+    "revision": "071853031a2175ada019db9e6fd1585c",
+    "url": "/fmlapp/static/media/DMSans-Bold.07185303.ttf"
+  },
+  {
+    "revision": "fbbc5a515be4021a9a36f048e25ad396",
+    "url": "/fmlapp/static/media/DMSans-Medium.fbbc5a51.ttf"
+  },
+  {
+    "revision": "3e7f038b85daa739336e4a3476c687f2",
+    "url": "/fmlapp/static/media/DMSans-Regular.3e7f038b.ttf"
+  },
+  {
+    "revision": "8a6aed6d0990394aef73953f7caa8533",
+    "url": "/fmlapp/static/media/Error.8a6aed6d.svg"
+  },
+  {
+    "revision": "b9452092a553851282cff5896c9293b9",
+    "url": "/fmlapp/static/media/Rectangle 593.b9452092.png"
+  },
+  {
+    "revision": "094999ca6faf73e89d4aeb605b486a6e",
+    "url": "/fmlapp/static/media/Rectangle 594.094999ca.png"
+  },
+  {
+    "revision": "144e2cfbe366402da76dd20aecc7c465",
+    "url": "/fmlapp/static/media/Rectangle 597.144e2cfb.png"
+  },
+  {
+    "revision": "1faeb2c867b82172dd928ea4760ba626",
+    "url": "/fmlapp/static/media/Successfully.1faeb2c8.svg"
+  },
+  {
+    "revision": "befeaf146bef40ac8efd555ac8b36487",
+    "url": "/fmlapp/static/media/a.befeaf14.png"
+  },
+  {
+    "revision": "3cf257e3ff34604a2a6f349f68261807",
+    "url": "/fmlapp/static/media/about.3cf257e3.png"
+  },
+  {
+    "revision": "ab47aa2915e2da697afe9cde8e0895df",
+    "url": "/fmlapp/static/media/arrow-icon.ab47aa29.svg"
+  },
+  {
+    "revision": "a901519852b19eff933b1f139cc3a7e1",
+    "url": "/fmlapp/static/media/arrow-left.a9015198.svg"
+  },
+  {
+    "revision": "dfe3fd80b61f9ac3efde4e3df2a9536d",
+    "url": "/fmlapp/static/media/arrow-right.dfe3fd80.svg"
+  },
+  {
+    "revision": "b597c4f6ec33678cfd82f8ea0519be47",
+    "url": "/fmlapp/static/media/bomb.b597c4f6.svg"
+  },
+  {
+    "revision": "8779e02a7764e8413be6f0479fa3e5b4",
+    "url": "/fmlapp/static/media/borrowers-stock.8779e02a.png"
+  },
+  {
+    "revision": "1cd047b97feba71b0f1485279d1adf5c",
+    "url": "/fmlapp/static/media/bx_bxl-facebook.1cd047b9.svg"
+  },
+  {
+    "revision": "af7a57865d29a9d80b630eaf948399f5",
+    "url": "/fmlapp/static/media/bx_bxl-twitter.af7a5786.svg"
+  },
+  {
+    "revision": "1b17e77ff67c4bbbc3577a6d791970d0",
+    "url": "/fmlapp/static/media/bx_bxs-copy.1b17e77f.svg"
+  },
+  {
+    "revision": "bcc98a90072ee99625abb8cc1eb6c19e",
+    "url": "/fmlapp/static/media/call-icon.bcc98a90.svg"
+  },
+  {
+    "revision": "d5bf0dccfdd9a812509e8d0debda7e5b",
+    "url": "/fmlapp/static/media/contact-text.d5bf0dcc.svg"
+  },
+  {
+    "revision": "afea358df9214efeceeaa3e43624d79d",
+    "url": "/fmlapp/static/media/credit-card.afea358d.svg"
+  },
+  {
+    "revision": "c68e5d2733242b64a9c1e4517881d84e",
+    "url": "/fmlapp/static/media/credit-score-bg-mobile.c68e5d27.svg"
+  },
+  {
+    "revision": "57d824105f06d37faa0c77e2dc520987",
+    "url": "/fmlapp/static/media/credit-score-bg-pc.57d82410.svg"
+  },
+  {
+    "revision": "4356accd839e494ec41de22dacaef7bd",
+    "url": "/fmlapp/static/media/email-icon.4356accd.svg"
+  },
+  {
+    "revision": "48af97c13402ec99b8de4b1ead5c07c2",
+    "url": "/fmlapp/static/media/facebook-icon.48af97c1.svg"
+  },
+  {
+    "revision": "943fb15d81fabda0a095bcc039ee1b5f",
+    "url": "/fmlapp/static/media/facebook-square-brands.943fb15d.svg"
+  },
+  {
+    "revision": "72acab8eab1ed222a6f6f7bf4da69f7c",
+    "url": "/fmlapp/static/media/facebook.72acab8e.svg"
+  },
+  {
+    "revision": "8a6aed6d0990394aef73953f7caa8533",
+    "url": "/fmlapp/static/media/failure.8a6aed6d.svg"
+  },
+  {
+    "revision": "bee102a4963eea400d66f61801546954",
+    "url": "/fmlapp/static/media/find-campaign-icon.bee102a4.svg"
+  },
+  {
+    "revision": "2322712f03bb559f3f8db4d2e660aa0c",
+    "url": "/fmlapp/static/media/footer-logo.2322712f.svg"
+  },
+  {
+    "revision": "f059397a9d59e0eeeb7b6036ee7cba82",
+    "url": "/fmlapp/static/media/fund-campaign-icon.f059397a.svg"
+  },
+  {
+    "revision": "1eadc70c46508060099713cee62517f3",
+    "url": "/fmlapp/static/media/get-returns-icon.1eadc70c.svg"
+  },
+  {
+    "revision": "b62d39c44b1d1876cfc3bce3c7a54021",
+    "url": "/fmlapp/static/media/googleicon.b62d39c4.svg"
+  },
+  {
+    "revision": "432f2ee5b1c889bca07e74df4eb79b01",
+    "url": "/fmlapp/static/media/head.432f2ee5.png"
+  },
+  {
+    "revision": "2232f553bdf88d665a6fff4cafff2b3e",
+    "url": "/fmlapp/static/media/how-it-works-bg.2232f553.jpg"
+  },
+  {
+    "revision": "42de59fa490dbc1a74876d3a0865730f",
+    "url": "/fmlapp/static/media/instagram-icon.42de59fa.svg"
+  },
+  {
+    "revision": "1312113c8d398ee376e6a098933833ae",
+    "url": "/fmlapp/static/media/instagram.1312113c.svg"
+  },
+  {
+    "revision": "b8a81f8fd4d7f63e398c48ce1fecf7be",
+    "url": "/fmlapp/static/media/landing-illustration.b8a81f8f.svg"
+  },
+  {
+    "revision": "5574a09ca1c0a668176ab0aa960bea6b",
+    "url": "/fmlapp/static/media/landing.5574a09c.png"
+  },
+  {
+    "revision": "e3a8a22c1b52f228ef0ade4f7e1c4d55",
+    "url": "/fmlapp/static/media/lenders-stock.e3a8a22c.png"
+  },
+  {
+    "revision": "314fb578a652f34b125f5765eeb9cdc8",
+    "url": "/fmlapp/static/media/linkedin-icon.314fb578.svg"
+  },
+  {
+    "revision": "2941b64b572cf7965dd4521edabf6a47",
+    "url": "/fmlapp/static/media/linkedin.2941b64b.svg"
+  },
+  {
+    "revision": "18b2fd1b6b94e1c9c58c42f95eb6dd57",
+    "url": "/fmlapp/static/media/location-icon.18b2fd1b.svg"
+  },
+  {
+    "revision": "5aeda87041d1c8f41cb839291ec80f2b",
+    "url": "/fmlapp/static/media/login-bg.5aeda870.png"
+  },
+  {
+    "revision": "75cad6d1a33382aeb186ca031c5962e2",
+    "url": "/fmlapp/static/media/logo.75cad6d1.svg"
+  },
+  {
+    "revision": "ba3f72b51dce4b4d21a5caba84289920",
+    "url": "/fmlapp/static/media/man-in-red-jacket-1681010.ba3f72b5.jpg"
+  },
+  {
+    "revision": "73c96085cb15d1b7f98189d3193aa9c2",
+    "url": "/fmlapp/static/media/money-growth1.73c96085.svg"
+  },
+  {
+    "revision": "79e6b86e101f559564320745e18f7240",
+    "url": "/fmlapp/static/media/nohistory.79e6b86e.svg"
+  },
+  {
+    "revision": "a4b5206f46bdde42187d80f74f8e0c95",
+    "url": "/fmlapp/static/media/paid-progress.a4b5206f.svg"
+  },
+  {
+    "revision": "e2c7855479ae4ec4adbc9298d8b6d6f0",
+    "url": "/fmlapp/static/media/placeholder.e2c78554.svg"
+  },
+  {
+    "revision": "98fda72c7199c8e593cd82d044b6e37b",
+    "url": "/fmlapp/static/media/plus.98fda72c.svg"
+  },
+  {
+    "revision": "386e71f60646fc43ed1fea0e8fc2bbff",
+    "url": "/fmlapp/static/media/ri_whatsapp-fill.386e71f6.svg"
+  },
+  {
+    "revision": "37270f1532a9de3654fe11f0d72a47ab",
+    "url": "/fmlapp/static/media/save-money.37270f15.svg"
+  },
+  {
+    "revision": "ef1e690893cd21fd8f68a1c270c11248",
+    "url": "/fmlapp/static/media/scale.ef1e6908.svg"
+  },
+  {
+    "revision": "e061f4655513e5d078d7727b16a53c3a",
+    "url": "/fmlapp/static/media/search.e061f465.svg"
+  },
+  {
+    "revision": "0228c5535a5c4b8536ce22a28cdbc7a0",
+    "url": "/fmlapp/static/media/sellers-stock.0228c553.png"
+  },
+  {
+    "revision": "09997af9111933601770c289fcb2e0c6",
+    "url": "/fmlapp/static/media/signup-bg.09997af9.png"
+  },
+  {
+    "revision": "1ff21156ebeb752147c0e09a61145ffd",
+    "url": "/fmlapp/static/media/subscribe.1ff21156.png"
+  },
+  {
+    "revision": "1faeb2c867b82172dd928ea4760ba626",
+    "url": "/fmlapp/static/media/success.1faeb2c8.svg"
+  },
+  {
+    "revision": "83b0d3bd1ea38e690d96c481b3c13cdd",
+    "url": "/fmlapp/static/media/test-back.83b0d3bd.png"
+  },
+  {
+    "revision": "2a03e0aea8830197f525a811ac56da5f",
+    "url": "/fmlapp/static/media/twitter-icon.2a03e0ae.svg"
+  },
+  {
+    "revision": "cf381cf3bb2120705a3bf2b004f7ecf4",
+    "url": "/fmlapp/static/media/twitter-icon.cf381cf3.svg"
+  },
+  {
+    "revision": "42548f99ad62472131b7cd38a2e6d956",
+    "url": "/fmlapp/static/media/twitter.42548f99.svg"
+  },
+  {
+    "revision": "51250a1aff71fbe1dbf47908837fa171",
+    "url": "/fmlapp/static/media/user2.51250a1a.png"
+  },
+  {
+    "revision": "ba0238585ea1572f7cee63114b087ef3",
+    "url": "/fmlapp/static/media/user3.ba023858.png"
+  },
+  {
+    "revision": "b0010cb5290e93555a8320dc83136fae",
+    "url": "/fmlapp/static/media/volume.b0010cb5.svg"
+  },
+  {
+    "revision": "4a8634511f90567b146b6cd8d2a6f941",
+    "url": "/fmlapp/static/media/woman.4a863451.png"
+  }
+]);
